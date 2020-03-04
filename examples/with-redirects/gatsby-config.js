b7557348bb2f8ac904b4f41aae927109b0fb9595
Jari Zwarts <jari@oberon.nl>
2020-03-04 18:28:35 +0100

    proxy agent & new recipe for proxy.

--- examples/with-redirects/gatsby-config.js
+++ examples/with-redirects/gatsby-config.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
     siteMetadata: {
         title: 'Gatsby Default Starter',
-        description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-        author: '@gatsbyjs'
+        description:
+            'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+        author: '@gatsbyjs',
     },
     plugins: [
         {
@@ -10,13 +13,13 @@ module.exports = {
             options: {
                 bucketName: 'gatsby-plugin-s3-' + process.env.AWS_STAGE,
                 region: 'eu-west-1',
-                generateRedirectObjectsForPermanentRedirects: true
+                generateRedirectObjectsForPermanentRedirects: true,
             },
         },
         {
             resolve: `gatsby-plugin-create-client-paths`,
             options: { prefixes: [`/client-only/*`] },
         },
-        'gatsby-plugin-offline'
-    ]
+        'gatsby-plugin-offline',
+    ],
 };
