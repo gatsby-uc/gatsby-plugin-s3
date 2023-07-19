208340ba0a32fbe98c82fd83b28246590d796b06
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2023-07-19 12:10:39 +1200

    fix: adjust dependencies and build errors

--- examples/with-redirects/src/pages/client-only/[...].js
+++ examples/with-redirects/src/pages/client-only/[...].js
@@ -2,16 +2,18 @@ import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
 import { Link } from 'gatsby';
 
-export default () => (
+const ClientOnlyPage = () => (
     <Fragment>
         Welcome to the app!
         <Link to="/client-only/secret">
             Here's a secret route that doesn't exist on the server
         </Link>
         <Router>
-            <Secret path="/client-only/secret" />
+            <Secret path="/client-only/secret"/>
         </Router>
     </Fragment>
 );
+
+export default ClientOnlyPage;
 
 const Secret = () => 'You\'re visiting me from the client side!';
